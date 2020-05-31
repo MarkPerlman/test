ae34cce1ced1e6e15393b211b8d0323b22dcd962
Mark Perlman <mark@DN0a1ea40d.SUNet>
2020-05-31 09:43:57 -0700

    added bump window frequency response

---
+++ bump_window.py
@@ -0,0 +1,8 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+w = np.ones(51)
+W = np.fft.fftshift(np.fft.fft(w,1024))
+
+plt.plot(20*np.log10(np.absolute(W)/max(np.absolute(W))))
+plt.show()
