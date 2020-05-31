0aab0c2370dbf1dbf22831301904d973b565651f
Mark Perlman <mark@DN0a1ea40d.SUNet>
2020-05-31 09:54:53 -0700

    added plot title

--- bump_window.py
+++ bump_window.py
@@ -5,4 +5,5 @@ w = np.ones(51)
 W = np.fft.fftshift(np.fft.fft(w,1024))
 
 plt.plot(20*np.log10(np.absolute(W)/max(np.absolute(W))))
+plt.title("Frequency response of rectangular window")
 plt.show()
